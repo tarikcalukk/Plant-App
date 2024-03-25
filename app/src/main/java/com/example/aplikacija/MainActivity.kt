157f47b1eef35c80749f5e3e5985f6aa389b4650
Tarik Caluk <tarik.caluk@hotmail.com>
2024-03-25 13:09:36 +0100

    Postavljanje spinnera i adaptera

--- app/src/main/java/com/example/aplikacija/MainActivity.kt
+++ app/src/main/java/com/example/aplikacija/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.aplikacija
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val spinner = findViewById<Spinner>(R.id.modSpinner)
+        val options = arrayListOf("Medicinski", "Kuharski", "Botanički")
+        val layoutID = android.R.layout.simple_spinner_item
+        val myAdapterInstance: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, layoutID, options)
+        spinner.setAdapter(myAdapterInstance)
+
     }
 }
